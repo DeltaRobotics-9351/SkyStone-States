4f61f202d43694367ae62127a5bcb9b3b11dcee2
Sebastian Erives Mejia <serivesmejia@gmail.com>
2020-02-05 20:25:26 -0700

    auton selector

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/rojo/AutonomoCompletoLadoPared.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import org.firstinspires.ftc.teamcode.autonomous.extender.Autonomo;
+
+public class AutonomoCompletoLadoPared extends Autonomo {
+}
