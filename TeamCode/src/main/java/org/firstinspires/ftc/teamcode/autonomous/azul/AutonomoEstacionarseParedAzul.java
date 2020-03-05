b89cf49d0bff118ab52e9d3db615065d4b2350e7
Sebastian Erives Mejia <serivesmejia@gmail.com>
2020-03-04 21:14:18 -0700

    albuquerque.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/azul/AutonomoEstacionarseParedAzul.java
@@ -0,0 +1,54 @@
+/*
+ * Created by FTC team Delta Robotics #9351
+ *  Source code licensed under the MIT License
+ *  More info at https://choosealicense.com/licenses/mit/
+ */
+
+package org.firstinspires.ftc.teamcode.autonomous.rojo;
+
+import com.deltarobotics9351.deltadrive.extendable.linearopmodes.mecanum.IMUPIDEncoderMecanumLinearOpMode;
+import com.deltarobotics9351.deltadrive.motors.andymark.NeveRest_Orbital_20;
+import com.deltarobotics9351.pid.PIDCoefficients;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.MotivateTelemetry;
+import org.firstinspires.ftc.teamcode.hardware.Hardware;
+
+@Autonomous(name="Estacionarse PARED Rojo", group="Rojo")
+public class AutonomoEstacionarseParedRojo extends IMUPIDEncoderMecanumLinearOpMode {
+
+    public Hardware hdw;
+
+    @Override
+    public void _runOpMode(){
+
+        setPID(new PIDCoefficients(0.0191, 0, 0));
+        imuParameters.DEAD_ZONE = 0.15;
+//encoders
+        encoderParameters.TICKS_PER_REV = NeveRest_Orbital_20.TICKS_PER_REVOLUTION;
+        encoderParameters.DRIVE_GEAR_REDUCTION = 1;
+        encoderParameters.WHEEL_DIAMETER_INCHES = 4;
+//esto te motiva
+        String[] s = MotivateTelemetry.doMotivateGlobal();
+        telemetry.addData(s[0], s[1]);
+
+        waitForStart();
+
+        if(!isStarted()) return;
+
+        strafeLeft(18, 0.2, 5);
+
+    }
+
+    @Override
+    public void setup(){
+        hdw = new Hardware(hardwareMap);
+        hdw.initHardware(false);
+
+        frontLeft = hdw.wheelFrontLeft;
+        frontRight = hdw.wheelFrontRight;
+        backLeft = hdw.wheelBackLeft;
+        backRight = hdw.wheelBackRight;
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/azul/AutonomoFoundationAzul.java
@@ -0,0 +1,38 @@
+/*
+ * Created by FTC team Delta Robotics #9351
+ *  Source code licensed under the MIT License
+ *  More info at https://choosealicense.com/licenses/mit/
+ */
+
+package org.firstinspires.ftc.teamcode.autonomous.azul;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.autonomous.rojo.AutonomoFoundationSkystoneRojo;
+import org.firstinspires.ftc.teamcode.hardware.Hardware;
+import org.firstinspires.ftc.teamcode.pipeline.SkystonePatternPipeline;
+
+@Autonomous(name = "Foundation Skystone Azul", group = "Azul")
+public class AutonomoFoundationSkystoneAzul extends AutonomoFoundationSkystoneRojo {
+
+    //esta clase extiende el autonomo completo rojo e invierte los giros, los deslices, y el OpenCV
+//tambien crashea y rompe el robot
+    @Override
+    public void setup(){
+        skystonePipeline.alliance = SkystonePatternPipeline.Alliance.BLUE; //le decimos al OpenCV que seremos alianza azul.
+//este es el resultado de una esclavisacion
+        imuParameters.INVERT_ROTATION = true; //invertimos las rotaciones
+
+        encoderParameters.RIGHT_WHEELS_STRAFE_TURBO = -1; //invertimos los deslices
+        encoderParameters.LEFT_WHEELS_STRAFE_TURBO = -1;
+
+        hdw = new Hardware(hardwareMap);
+        hdw.initHardware(false);
+
+        frontLeft = hdw.wheelFrontLeft;
+        frontRight = hdw.wheelFrontRight;
+        backLeft = hdw.wheelBackLeft;
+        backRight = hdw.wheelBackRight;
+    }
+
+}
